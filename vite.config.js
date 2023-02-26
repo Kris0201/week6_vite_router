cc0606a3ba6ce2d113e8c108531b3d1f701f46b2
Kris Tung <ragdoll.washoku89@gmail.com>
2023-02-26 22:03:39 +0800

    建立 deploy.sh & 在vite.config.js 中新增 base

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/week6_vite_router/", // Github repo 名稱
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  alias: {
+    '@': fileURLToPath(new URL('./src', import.meta.url))
   }
+}
 })
