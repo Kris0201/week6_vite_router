09da1828fdf6ec97b040b78d689a042c58d2857e
Kris Tung <ragdoll.washoku89@gmail.com>
2023-02-26 21:05:37 +0800

    vite 專案佈署至 gh-pages

---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    this.$http.get('https://randomuser.me/api/').then((res) => {
+      console.log(res);
+      this.data=res.data.results[0]
+    }) // 測試 vue-axios 是否正確取得遠端資料
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
