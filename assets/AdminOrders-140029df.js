7f88d7de712921c2ca86dde4d758f1e43f6b6be1
Kris Tung <ragdoll.washoku89@gmail.com>
2023-03-05 21:56:12 +0800

    npm run build

---
+++ assets/AdminOrders-140029df.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r}from"./index-d92b34dd.js";const t={};function c(n,s,a,_,p,d){return o(),r("div",null," 我是後台訂單管理頁面 ")}const i=e(t,[["render",c]]);export{i as default};
---
+++ assets/AdminProducts-1935d49a.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./index-d92b34dd.js";const c={};function n(r,s,a,_,p,d){return o(),t("div",null," 這是後台產品管理頁面 ")}const i=e(c,[["render",n]]);export{i as default};
